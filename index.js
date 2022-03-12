7064341a3137d2d89fbf20b07c914f426e468963
Gabriel Henrique Custodio <bielriquecustodio@gmail.com>
2022-03-11 22:36:03 -0300

    This is my commit to add id to users and create the update and delete routes.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express')
+const uuid = require('uuid')
 const app = express()
 const port = 3000
 const users =[]
+
+const checkUser = ( request , response , next ) => {
+    const { id } = request.params 
+    const index = users.findIndex( user => user.id === id)
+
+    if( index < 0 ){
+        return response.status(404).json( { error : "User not found" } )
+    }
+
+    request.index = index
+    request.id = id
+
+    next()
+}
 
 app.use(express.json())
 
@@ -11,11 +26,29 @@ app.get('/users' , ( request , response ) => {
 
 app.post('/users' , ( request , response) => {
     const { name , age } = request.body
-    const user = { name , age}
+    const user = {id:uuid.v4() ,name , age }
 
     users.push(user)
 
     return response.json(user)
+})
+
+app.put('/users/:id' , checkUser , ( request , response) => {
+    const { name , age } = request.body
+    const index = request.index
+    const id = request.id
+
+    const updateUser = { id , name , age }
+
+    users[index] = updateUser
+    return response.json(updateUser)
+})
+
+app.delete('/users/:id' , checkUser , ( request , response ) => {
+    const index = request.index
+    users.splice(index , 1)
+
+    return response.status(204).json()
 })
 
 app.listen( port , () => {
